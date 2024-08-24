7ebd34b1e071f7b2684d941e4da46be48bc1d01d
Nishant <vikaskumar.e1256@gmail.com>
2024-08-24 08:35:04 +0530

    database connection and mongoose package installed

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,27 @@
 const express = require('express');
-const app = express();
 require("dotenv").config();
+const mongoose = require('mongoose');
+
+const app = express();
+
+
+// Database connection
+main().catch(err => console.log(err));
+
+async function main()
+{
+    await mongoose.connect('mongodb://127.0.0.1:27017/mern_ecommerce');
+
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
 
 app.get("/", (req, res) =>
 {
     res.send("hello world");
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () =>
 {
