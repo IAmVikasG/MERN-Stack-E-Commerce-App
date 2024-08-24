caf4e1f17a0250c69f3dfd1dafb5ede77e0d4a30
Nishant <vikaskumar.e1256@gmail.com>
2024-08-24 16:01:59 +0530

    requiresignin middleware

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,6 +3,11 @@ const jwt = require("jsonwebtoken"); // to generate signed token
 const expressJwt = require("express-jwt"); // for authorization check
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ["HS256"], // added later
+    userProperty: "auth",
+});
 
 exports.signup = (req, res) =>
 {
