a4fd180639b3ca5ae83aed3733dd7b0544956080
Nishant <vikaskumar.e1256@gmail.com>
2024-08-24 15:39:45 +0530

    user signout

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 // Custom imports file
-const { signup, signin } = require('../controllers/user');
+const { signup, signin, signout } = require('../controllers/user');
 const { userSignupValidator, userSigninValidator } = require('../validator/index');
 
 router.post('/signup', userSignupValidator, signup);
 router.post('/signin', userSigninValidator, signin);
+router.get('/signout', signout);
 
 module.exports = router;
