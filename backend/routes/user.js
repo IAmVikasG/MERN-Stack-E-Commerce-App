90a879183038711efc365b7efa779cd5ee0ee871
Nishant <vikaskumar.e1256@gmail.com>
2024-08-24 08:49:19 +0530

    basic controller structure defined

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) =>
-{
-    res.send('Hello World');
-})
+// Custom imports file
+const { index } = require('../controllers/user');
+
+router.get('/', index);
 
 module.exports = router;
