f463acd541b9444cf083a6913da1b061ba37e368
Nishant <vikaskumar.e1256@gmail.com>
2024-08-25 14:45:22 +0530

    Fetch related products

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, productById, read, remove, update, list } = require("../controllers/product");
+const { create, productById, read, remove, update, list, listRelated } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
@@ -22,6 +22,7 @@ router.delete(
     remove
 );
 router.get("/products", list);
+router.get("/products/related/:productId", listRelated);
 
 
 router.param("userId", userById);
