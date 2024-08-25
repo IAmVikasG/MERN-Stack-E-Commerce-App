6d934159c2cdac24f15f7a31b984cb730d80f39f
Nishant <vikaskumar.e1256@gmail.com>
2024-08-25 20:25:48 +0530

    Private route for authenticated users only

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Signup from "./auth/Signup";
 import Signin from "./auth/Signin";
 import Home from "./core/Home";
+import Dashboard from "./user/UserDashboard";
+import PrivateRoute from './routes/PrivateRoute';
 
 const Routes = () => {
     return (
@@ -11,6 +13,7 @@ const Routes = () => {
                 <Route path="/" exact component={Home} />
                 <Route path="/signin" exact component={Signin} />
                 <Route path="/signup" exact component={Signup} />
+                <PrivateRoute path="/dashboard" exact component={Dashboard} />
             </Switch>
         </BrowserRouter>
     );
