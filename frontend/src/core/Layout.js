d13f076f0f2c40f757fbdfe217d2d9466be9b45c
Nishant <vikaskumar.e1256@gmail.com>
2024-08-25 16:47:13 +0530

    shared layout and active links

---
+++ frontend/src/core/Layout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Menu from "./Menu";
+
+const Layout = ({
+    title = "Title",
+    description = "Description",
+    className,
+    children
+}) => (
+    <React.Fragment>
+        <Menu />
+        <div className="jumbotron">
+            <h2>{title}</h2>
+            <p className="lead">{description}</p>
+        </div>
+        <div className={className}>{children}</div>
+    </React.Fragment>
+);
+
+export default Layout;
