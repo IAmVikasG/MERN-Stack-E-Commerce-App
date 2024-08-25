3b95a8e040ea7900423c9c5cff47b6f2e1bd2d85
Nishant <vikaskumar.e1256@gmail.com>
2024-08-25 15:30:34 +0530

    Cors package

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator');
+const cors = require("cors");
 
 
 const app = express();
@@ -30,6 +31,8 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
+
 
 // Routes
 app.use('/api', authRoutes);
