ca4a662ad38027250371d480ad3cc6953a42c50f
Nishant <vikaskumar.e1256@gmail.com>
2024-08-26 10:19:46 +0530

    filter with price range

--- frontend/src/core/Shop.js
+++ frontend/src/core/Shop.js
@@ -3,40 +3,53 @@ import Layout from "./Layout";
 import Card from "./Card";
 import { getCategories } from "./apiCore";
 import Checkbox from "./Checkbox";
+import RadioBox from "./RadioBox";
+import { prices } from "./fixedPrices";
 
-const Shop = () =>
-{
+const Shop = () => {
     const [myFilters, setMyFilters] = useState({
         filters: { category: [], price: [] }
     });
     const [categories, setCategories] = useState([]);
     const [error, setError] = useState(false);
 
-    const init = () =>
-    {
-        getCategories().then(data =>
-        {
-            if (data.error)
-            {
+    const init = () => {
+        getCategories().then(data => {
+            if (data.error) {
                 setError(data.error);
-            } else
-            {
+            } else {
                 setCategories(data);
             }
         });
     };
 
-    useEffect(() =>
-    {
+    useEffect(() => {
         init();
     }, []);
 
-    const handleFilters = (filters, filterBy) =>
-    {
+    const handleFilters = (filters, filterBy) => {
         // console.log("SHOP", filters, filterBy);
         const newFilters = { ...myFilters };
         newFilters.filters[filterBy] = filters;
+
+        if (filterBy === "price") {
+            let priceValues = handlePrice(filters);
+            newFilters.filters[filterBy] = priceValues;
+        }
+
         setMyFilters(newFilters);
+    };
+
+    const handlePrice = value => {
+        const data = prices;
+        let array = [];
+
+        for (let key in data) {
+            if (data[key]._id === parseInt(value)) {
+                array = data[key].array;
+            }
+        }
+        return array;
     };
 
     return (
@@ -56,6 +69,16 @@ const Shop = () =>
                             }
                         />
                     </ul>
+
+                    <h4>Filter by price range</h4>
+                    <div>
+                        <RadioBox
+                            prices={prices}
+                            handleFilters={filters =>
+                                handleFilters(filters, "price")
+                            }
+                        />
+                    </div>
                 </div>
 
                 <div className="col-8">{JSON.stringify(myFilters)}</div>
