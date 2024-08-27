03abebb5841fb90db30e2e6540aa73e81043ee67
Nishant <vikaskumar.e1256@gmail.com>
2024-08-27 16:32:50 +0530

    backend implementation of search

--- frontend/src/core/Search.js
+++ frontend/src/core/Search.js
@@ -64,6 +64,17 @@ const Search = () =>
         setData({ ...data, [name]: event.target.value, searched: false });
     };
 
+    const searchedProducts = (results = []) =>
+    {
+        return (
+            <div className="row">
+                {results.map((product, i) => (
+                    <Card key={i} product={product} />
+                ))}
+            </div>
+        );
+    };
+
     const searchForm = () => (
         <form onSubmit={searchSubmit}>
             <span className="input-group-text">
@@ -101,12 +112,13 @@ const Search = () =>
 
     return (
         <div className="row">
-            <div className="container mb-3">
-                {searchForm()}
-                {JSON.stringify(results)}
+            <div className="container mb-3">{searchForm()}</div>
+            <div className="container-fluid mb-3">
+                {searchedProducts(results)}
             </div>
         </div>
     );
 };
 
 export default Search;
+
