98353d635c740c9b98279d80b4a8b8d08f538a33
Nishant <vikaskumar.e1256@gmail.com>
2024-08-28 14:39:06 +0530

    braintree gateway setup for frontend

--- frontend/src/core/Checkout.js
+++ frontend/src/core/Checkout.js
@@ -1,28 +1,76 @@
 import React, { useState, useEffect } from "react";
 import { useCart } from "react-use-cart";
 import Layout from "./Layout";
-import { getProducts } from "./apiCore";
-import Card from "./Card";
+import { getBraintreeClientToken } from "./apiCore";
 import { isAuthenticated } from "../API/auth";
 import { Link } from "react-router-dom";
+import DropIn from "braintree-web-drop-in-react";
 
 const Checkout = () =>
 {
-    const { cartTotal } = useCart();
+    const { cartTotal, items, isEmpty } = useCart();
+
+    const [data, setData] = useState({
+        success: false,
+        clientToken: null,
+        error: "",
+        instance: {},
+        address: ""
+    });
+
+    const userId = isAuthenticated() && isAuthenticated().user._id;
+    const token = isAuthenticated() && isAuthenticated().token;
+
+    const getToken = (userId, token) =>
+    {
+        getBraintreeClientToken(userId, token).then(data =>
+        {
+            if (data.error)
+            {
+                setData({ ...data, error: data.error });
+            } else
+            {
+                setData({ ...data, clientToken: data.clientToken });
+            }
+        });
+    };
+
+    useEffect(() =>
+    {
+        getToken(userId, token);
+    }, []);
+
 
     const getTotal = () => {
         return cartTotal;
     };
 
-    const showCheckout = () => {
+    const showCheckout = () =>
+    {
         return isAuthenticated() ? (
-            <button className="btn btn-success">Checkout</button>
+            <div>{showDropIn()}</div>
         ) : (
             <Link to="/signin">
                 <button className="btn btn-primary">Sign in to checkout</button>
             </Link>
         );
     };
+
+    const showDropIn = () => (
+        <div>
+            {data.clientToken !== null && !isEmpty && items.length > 0 ? (
+                <div>
+                    <DropIn
+                        options={{
+                            authorization: data.clientToken
+                        }}
+                        onInstance={instance => (data.instance = instance)}
+                    />
+                    <button className="btn btn-success">Checkout</button>
+                </div>
+            ) : null}
+        </div>
+    );
 
     return (
         <div>
