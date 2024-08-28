94fcb868af6f0883a4e72e8fe6ae9a339d0c62f3
Nishant <vikaskumar.e1256@gmail.com>
2024-08-28 20:53:36 +0530

    Update order status by admin

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -1,6 +1,23 @@
 const { Order, CartItem } = require("../models/order");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+exports.orderById = (req, res, next, id) =>
+{
+    Order.findById(id)
+        .populate("products.product", "name price")
+        .exec((err, order) =>
+        {
+            if (err || !order)
+            {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            req.order = order;
+            next();
+        });
+};
+
 exports.create = (req, res) =>
 {
     // console.log("CREATE ORDER: ", req.body);
@@ -38,4 +55,22 @@ exports.listOrders = (req, res) =>
 exports.getStatusValues = (req, res) =>
 {
     res.json(Order.schema.path("status").enumValues);
+};
+
+exports.updateOrderStatus = (req, res) =>
+{
+    Order.update(
+        { _id: req.body.orderId },
+        { $set: { status: req.body.status } },
+        (err, order) =>
+        {
+            if (err)
+            {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(order);
+        }
+    );
 };
