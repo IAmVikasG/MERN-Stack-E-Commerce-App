ae481215824754f0191f4e897db9212d17cbeec2
Nishant <vikaskumar.e1256@gmail.com>
2024-08-28 20:15:33 +0530

    Update sold products quantity

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -401,3 +401,27 @@ exports.listSearch = (req, res) =>
         }).select("-photo");
     }
 };
+
+exports.decreaseQuantity = (req, res, next) =>
+{
+    let bulkOps = req.body.order.products.map(item =>
+    {
+        return {
+            updateOne: {
+                filter: { _id: item._id },
+                update: { $inc: { quantity: -item.quantity, sold: +item.quantity } }
+            }
+        };
+    });
+
+    Product.bulkWrite(bulkOps, {}, (error, products) =>
+    {
+        if (error)
+        {
+            return res.status(400).json({
+                error: "Could not update product"
+            });
+        }
+        next();
+    });
+};
